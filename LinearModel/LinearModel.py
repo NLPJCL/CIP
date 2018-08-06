776d78c01a6296aed1cd3cbd584a7152d7f94c5d
Wu Kun <38521583+ThisIsSoMe@users.noreply.github.com>
2018-08-06 19:10:11 +0800

    Update LinearModel.py

--- LinearModel/LinearModel.py
+++ LinearModel/LinearModel.py
@@ -1,38 +1,44 @@
-#!\usr\bin\python
+#!/usr/bin/python
 # coding=UTF-8
+import numpy as np
 
 class linear_model:
-    sentences = []  # 句子的列表
-    pos = []  # words对应的词性
-    words = []  # 句子分割成词的词组列表的列表
-    dic_feature = {}  # 特征向量的字典
-    dic_tags = {}  # 词性的集合
-    def _init_(self):
+    def __init__(self):
         self.sentences=[] # 句子的列表
         self.pos=[] # words对应的词性
         self.words=[] # 句子分割成词的词组列表的列表
+        self.words_dev=[]
+        self.pos_dev=[]
         self.dic_feature={} # 特征向量的字典
         self.dic_tags={} # 词性的集合
+        self.tags=[]  # 词性的列表
+        self.len_feature=0
+        self.len_tags=0
+        self.weight=[] #  特征权重
+        self.v=[] #  权重累加
 
-    def readfile(self):
-        with open("train.conll",'r') as ft:
+    def readfile(self,filename):
+        words=[]
+        pos=[]
+        with open(filename,'r') as ft:
             temp_words = []
-            temp_sen = ''
             temp_poss = []
             for line in ft:
                 if len(line)>1:
                     temp_word=line.strip().split('\t')[1].decode('utf-8')
                     temp_pos=line.strip().split('\t')[3]
-                    temp_sen+=temp_word
                     temp_words.append(temp_word)
                     temp_poss.append(temp_pos)
                 else:
-                    self.words.append(temp_words)
-                    self.sentences.append(temp_sen)
-                    self.pos.append(temp_poss)
+                    words.append(temp_words)
+                    pos.append(temp_poss)
                     temp_words = []
-                    temp_sen = ''
                     temp_poss = []
+        return words,pos
+
+    def getdata(self):
+        self.words,self.pos=self.readfile('train.conll')
+        self.words_dev,self.pos_dev=self.readfile('dev.conll')
 
     def create_feature_templates(self,words,index,tag):
         f=[]
@@ -68,62 +74,76 @@ class linear_model:
                 f.append('13:'+str(tag)+'*'+wos[index][i]+'*'+'consecutive')
         if len(wos[index])==1:
             f.append('12:'+str(tag)+'*'+ci_minus_minus1+'*'+ci_plus0)
-        for i in range(1,len(wos[index])+1):
-            if i>4:
+        for i in range(0,len(wos[index])):
+            if i>=4:
                 break
             f.append('14:'+str(tag)+'*'+wos[index][0:i+1])
-            f.append('15:'+str(tag)+'*'+wos[index][-i-1:-1])
+            f.append('15:'+str(tag)+'*'+wos[index][-(i+1)::])
         return f
 
     def create_feature_space(self):
-        for i in range(0,len(self.sentences)):
-            for j in range(0,len(self.words[i])):
-                f=self.create_feature_templates(self.words[i],j,self.pos[i][j])
+        for i in range(0,len(self.words)):
+            cur_words=self.words[i]
+            for j in range(0,len(cur_words)):
+                tag=self.pos[i][j]
+                f=self.create_feature_templates(cur_words,j,tag)
                 for feat in f:
-                    self.dic_feature[feat]=0
-                if self.pos[i][j] in self.dic_tags:
-                    self.dic_tags[self.pos[i][j]]+=1
-                else:
-                    self.dic_tags[self.pos[i][j]] = 0
-        print('特征空间维度：%d'%len(self.dic_feature))
-        print ('词性维度：%d'%len(self.dic_tags))
+                    if feat not in self.dic_feature:
+                        self.dic_feature[feat]=len(self.dic_feature)
+                if tag not in self.tags:
+                    self.tags.append(tag)
+        self.len_feature=len(self.dic_feature)
+        self.len_tags=len(self.tags)
+        self.weight=np.zeros(self.len_feature)
+        self.v=np.zeros(self.len_feature)
+        self.dic_tags={value:i for i,value in enumerate(self.tags)}
+        print('特征空间维度：%d'%self.len_feature)
+        print ('词性维度：%d'%self.len_tags)
 
     def get_score(self,f):
-        score=0
-        for i in f :
-            if i in self.dic_feature:
-                score+=self.dic_feature[i]
-        return score
+        f_index=[self.dic_feature[fi] for fi in f if fi in self.dic_feature]
+        return np.sum(self.weight[f_index])
+
+    def get_score_average(self,f):
+        f_index = [self.dic_feature[fi] for fi in f if fi in self.dic_feature]
+        return np.sum(self.v[f_index])
 
     def get_max_tag(self,words,index):
-        max_score=-10
-        tag='NULL'
-        for t in self.dic_tags:
-            f=self.create_feature_templates(words,index,t)
-            temp_score=self.get_score(f)
-            if temp_score>max_score:
-                max_score=temp_score
-                tag=t
-        return tag
+        temp_score=np.zeros(self.len_tags)
+        for t in range(self.len_tags):
+            f=self.create_feature_templates(words,index,self.tags[t])
+            temp_score[t]=self.get_score(f)     # 权重不累加
+            #temp_score[t]=self.get_score_average(f)  # 权重累加
+        index=np.argmax(temp_score)
+        return self.tags[index]
 
-    def online_training(self):
-        for iteration in range(0,20):
-            print '当前迭代：'+str(iteration+1)
-            for index_sen in range(0,len(self.sentences)):
-                for index_word in range(0,len(self.words[index_sen])):
-                    #f=self.create_feature_templates()
-                    max_tag=self.get_max_tag(self.words[index_sen],index_word)
+    def get_max_tag_dev(self,words,index):
+        temp_score=np.zeros(self.len_tags)
+        for t in range(self.len_tags):
+            f=self.create_feature_templates(words,index,self.tags[t])
+            #temp_score[t]=self.get_score(f)     # 权重不累加
+            temp_score[t]=self.get_score_average(f)  # 权重累加
+        index=np.argmax(temp_score)
+        return self.tags[index]
+
+    def online_training(self,iteration=20):
+        for it in range(0,iteration):
+            print '当前迭代：'+str(it)
+            for index_sen in range(0,len(self.words)):
+                cur_sen=self.words[index_sen]
+                for index_word in range(0,len(cur_sen)):
+                    max_tag=self.get_max_tag(cur_sen,index_word)
                     right_tag=self.pos[index_sen][index_word]
                     if max_tag!=right_tag:
-                         f_right_tag=self.create_feature_templates(self.words[index_sen],index_word,right_tag)
-                         f_max_tag=self.create_feature_templates(self.words[index_sen],index_word,max_tag)
-                         for i in f_right_tag:
-                             if i in self.dic_feature:
-                                 self.dic_feature[i]+=1
-                         for i in f_max_tag:
-                             if i in self.dic_feature:
-                                 self.dic_feature[i]-=1
+                         f_right_tag=self.create_feature_templates(cur_sen,index_word,right_tag)
+                         f_max_tag=self.create_feature_templates(cur_sen,index_word,max_tag)
+                         f_right_index=[self.dic_feature[i] for i in f_right_tag]
+                         self.weight[f_right_index]+=1
+                         f_max_index = [self.dic_feature[i] for i in f_max_tag if i in self.dic_feature]
+                         self.weight[f_max_index] -= 1
+                         self.v+=self.weight
             self.test('train.conll')
+            self.test('dev.conll')
 
     def output(self):
         with open('model.txt','w+') as fm:
@@ -133,10 +153,9 @@ class linear_model:
 
     def test_sentence(self,words,tags):
         right=0
-        sumup=0
-        for i in range(0,len(words)):
-            max_tag=self.get_max_tag(words,i)
-            sumup+=1
+        sumup=len(words)
+        for i in range(0,sumup):
+            max_tag=self.get_max_tag_dev(words,i)
             if max_tag==tags[i]:
                 right+=1
         return right,sumup
@@ -144,20 +163,18 @@ class linear_model:
     def test(self,filename):
         right=0
         total=0
-        with open(filename,'r') as ft:
-            temp_words=[]
-            temp_pos=[]
-            for line in ft:
-                if len(line)>1:
-                    str_line=line.strip().split('\t')
-                    temp_words.append(str_line[1].decode('utf-8'))
-                    temp_pos.append(str_line[3])
-                else:
-                    sen_right,sen_len=self.test_sentence(temp_words,temp_pos)
-                    right+=sen_right
-                    total+=sen_len
-                    temp_words=[]
-                    temp_pos=[]
+        words=[]
+        pos=[]
+        if filename=='train.conll':
+            words=self.words
+            pos=self.pos
+        else:
+            words=self.words_dev
+            pos=self.pos_dev
+        for w,p in zip(words,pos):
+            r,t=self.test_sentence(w,p)
+            right+=r
+            total+=t
         pricision=1.0*right/total
         print('正确：'+str(right)+'总数：'+str(total)+'正确率:'+str(pricision))
         with open('result.txt','a+') as fr:
@@ -165,8 +182,7 @@ class linear_model:
 
 if __name__=='__main__':
     lm = linear_model()
-    lm.readfile()
+    lm.getdata()
     lm.create_feature_space()
-    lm.online_training()
+    lm.online_training(40)
     lm.output()
-    lm.test('dev.conll')
